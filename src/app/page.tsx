0b73fc55e38092fea0a62c620bce2e5e8c9570e0
“anishkumar127” <anishbishnoi127@gmail.com>
2023-10-29 21:26:49 +0530

    adding toolbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import { Menu } from "@/components/Menu/Menu";
+import { Toolbox } from "@/components/Toolbox/Toolbox";
 
 export default function Home() {
   return (
     <main>
       {/* jai shree ram */}
-      <Menu/>
+      {/* Menubar */}
+      <Menu />
+      {/* Toolbar */}
+      <Toolbox />
     </main>
-  )
+  );
 }
