c8989297db152f3c99a7585a902c4071feb71d3a
“anishkumar127” <anishbishnoi127@gmail.com>
2023-10-29 21:49:30 +0530

    toolbox & colors added.

--- src/components/Toolbox/Toolbox.tsx
+++ src/components/Toolbox/Toolbox.tsx
@@ -1,17 +1,48 @@
-'use client';
+"use client";
+import { COLORS } from "@/constants";
+import styles from "./toolbox.module.css";
 export const Toolbox = () => {
   const updateBrushSize = () => {};
   return (
-    <div>
-      <h4>Stroke Color</h4>
-      <div>
-        <div />
+    <div className={styles.toolboxContainer}>
+      <div className={styles.toolItem}>
+        <h4 className={styles.toolText}>Stroke Color</h4>
+        <div className={styles.itemContainer}>
+          <div
+            className={styles.colorBox}
+            style={{ backgroundColor: COLORS.BLACK }}
+          />
+          <div
+            className={styles.colorBox}
+            style={{ backgroundColor: COLORS.BLUE }}
+          />
+          <div
+            className={styles.colorBox}
+            style={{ backgroundColor: COLORS.GREEN }}
+          />
+          <div
+            className={styles.colorBox}
+            style={{ backgroundColor: COLORS.ORANGE }}
+          />
+          <div
+            className={styles.colorBox}
+            style={{ backgroundColor: COLORS.RED }}
+          />
+          {/* <div
+            className={styles.colorBox}
+            style={{ backgroundColor: COLORS.WHITE }}
+          /> */}
+          <div
+            className={styles.colorBox}
+            style={{ backgroundColor: COLORS.YELLOW }}
+          />
+        </div>
       </div>
 
       {/* Brush Size */}
-      <div>
-        <h4>Brush Size</h4>
-        <div>
+      <div className={styles.toolItem}>
+        <h4 className={styles.toolText}>Brush Size</h4>
+        <div className={styles.itemContainer}>
           <input
             type="range"
             min={1}
