eb66985b49a883ff3891156a3cec7b707677182b
“anishkumar127” <anishbishnoi127@gmail.com>
2023-11-01 23:27:18 +0530

    fixed typescript issue

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -4,8 +4,25 @@ import { useEffect, useRef } from "react";
 
 export const Board = () => {
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
-  const activeMenuItem = useAppSelector((state)=>state?.menu?.activeMenuItem);
-  const {color,size} = useAppSelector((state)=>state?.toolbox[activeMenuItem]);
+  const activeMenuItem = useAppSelector((state) => state?.menu?.activeMenuItem);
+  const { color, size } = useAppSelector(
+    (state) => state?.toolbox[activeMenuItem]
+  );
+
+  useEffect(() => {
+    if (!canvasRef?.current) return;
+
+    const canvas: HTMLCanvasElement = canvasRef?.current;
+    const context: CanvasRenderingContext2D | null | any =
+      canvas?.getContext("2d");
+    const changeConfig = () => {
+      if (context) {
+        context.strokeStyle = color;
+        context.lineWidth = size;
+      }
+    };
+    changeConfig();
+  }, [color, size]);
 
   useEffect(() => {
     if (!canvasRef?.current) return;
