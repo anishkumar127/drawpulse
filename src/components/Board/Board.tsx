34f5784764e7a0e5e89524607808feddf4d1bcba
“anishkumar127” <anishbishnoi127@gmail.com>
2023-11-06 22:45:46 +0530

    added features & ability to download drawed item

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -1,18 +1,41 @@
 "use client";
-import { useAppSelector } from "@/store/hooks";
+import { MENU_ITMES } from "@/constants";
+import { onActionItem } from "@/store/features/menuSlice";
+import { useAppDispatch, useAppSelector } from "@/store/hooks";
 import { useEffect, useLayoutEffect, useRef } from "react";
 
 export const Board = () => {
+  const dispatch = useAppDispatch();
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
   const shouldDraw = useRef<HTMLCanvasElement | boolean>(false);
-  const activeMenuItem = useAppSelector((state) => state?.menu?.activeMenuItem);
+  const { activeMenuItem, actionMenuItem } = useAppSelector(
+    (state) => state?.menu
+  );
   const { color, size } = useAppSelector(
     (state) => state?.toolbox[activeMenuItem]
   );
 
+  // download png
   useEffect(() => {
     if (!canvasRef?.current) return;
+    const canvas: HTMLCanvasElement = canvasRef?.current;
+    const context: CanvasRenderingContext2D | null | any =
+      canvas?.getContext("2d");
 
+    if (actionMenuItem === MENU_ITMES.DOWNLOAD) {
+      const URL = canvas.toDataURL();
+      const anchor = document.createElement("a");
+      anchor.href = URL;
+      anchor.download = "sketch.png";
+      anchor.click();
+    }
+    // return ()=>{
+    dispatch(onActionItem(null));
+    // }
+  }, [actionMenuItem, dispatch]);
+
+  useEffect(() => {
+    if (!canvasRef?.current) return;
     const canvas: HTMLCanvasElement = canvasRef?.current;
     const context: CanvasRenderingContext2D | null | any =
       canvas?.getContext("2d");
